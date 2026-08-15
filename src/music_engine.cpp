bf269f800a0b2181b63c7d7b10d41bc170477ab1
xXQuasarXx <stelarfield@protonmail.com>
2026-08-15 14:27:00 -0300

    0.1.1 - Miniaudio separation

--- src/music_engine.cpp
+++ src/music_engine.cpp
@@ -1,49 +1,61 @@
 #define MINIAUDIO_IMPLEMENTATION
 #include "music_engine.hpp"
 
-void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount){
-    ma_decoder* pDecoder = (ma_decoder*)pDevice->pUserData;
-    if(pDecoder == NULL){
-        return;
-    }
+#include <thread>
 
-    ma_decoder_read_pcm_frames(pDecoder, pOutput, frameCount, 0);
+struct AudioData{
+    ma_decoder decoder;
+    bool isFinished = false;
+};
+
+void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount){
+    AudioData* pAudioData = (AudioData*)pDevice->pUserData;
+    if(pAudioData == NULL) return;
+
+    ma_uint64 framesRead = 0;
+    ma_data_source_read_pcm_frames(&pAudioData->decoder, pOutput, frameCount, &framesRead);
+
+    if(framesRead < frameCount){
+        pAudioData->isFinished = true;
+    }
 
     (void)pInput;
 }
 
 void play_music(std::string music_url){
-    ma_decoder decoder;
-    ma_result result = ma_decoder_init_file(music_url.c_str(), NULL, &decoder);
+    AudioData audio_data;
+
+    ma_result result = ma_decoder_init_file(music_url.c_str(), NULL, &audio_data.decoder);
     if(result != MA_SUCCESS){
         std::cout << "ERROR: Unable to initiate file\n";
         return;
     }
 
     ma_device_config config = ma_device_config_init(ma_device_type_playback);
-    config.playback.format = decoder.outputFormat;
-    config.playback.channels = decoder.outputChannels;
-    config.sampleRate = decoder.outputSampleRate;
+    config.playback.format = audio_data.decoder.outputFormat;
+    config.playback.channels = audio_data.decoder.outputChannels;
+    config.sampleRate = audio_data.decoder.outputSampleRate;
     config.dataCallback = data_callback;
-    config.pUserData = &decoder;
+    config.pUserData = &audio_data;
 
     ma_device device;
     if(ma_device_init(NULL, &config, &device) != MA_SUCCESS){
         std::cout << "ERROR: Failed to open playback device\n";
-        ma_decoder_uninit(&decoder);
+        ma_decoder_uninit(&audio_data.decoder);
         return;
     }
 
     if(ma_device_start(&device) != MA_SUCCESS){
         std::cout << "ERROR: Failed to start playback device\n";
         ma_device_uninit(&device);
-        ma_decoder_uninit(&decoder);
+        ma_decoder_uninit(&audio_data.decoder);
         return;
     }
 
-    std::cout << "Press [ENTER] to exit...\n";
-    getchar();
+    while(!audio_data.isFinished){
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
 
     ma_device_uninit(&device);
-    ma_decoder_uninit(&decoder);
+    ma_decoder_uninit(&audio_data.decoder);
 }
