76d2e84991d41a4dff6c70fb88190b3afabc5ef3
xXQuasarXx <stelarfield@protonmail.com>
2026-08-18 19:21:25 -0300

    0.1.5c - More prep for daemon logic

--- main_cli.cpp
+++ main_cli.cpp
@@ -7,12 +7,42 @@
 #include <stdexcept>
 #include <filesystem>
 #include <chrono>
+#include <windows.h>
+#include <tlhelp32.h>
+#include <string_view>
+
+bool IsProcessRunning(std::wstring_view processName) {
+    bool exists = false;
+    PROCESSENTRY32W entry;
+    entry.dwSize = sizeof(PROCESSENTRY32W);
+
+    // Take a snapshot of all running processes
+    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+    if (snapshot == INVALID_HANDLE_VALUE) return false;
+
+    // Retrieve information about the first process
+    if (Process32FirstW(snapshot, &entry)) {
+        do {
+            // Compare current process name against target
+            if (processName == entry.szExeFile) {
+                exists = true;
+                break;
+            }
+        } while (Process32NextW(snapshot, &entry)); // Move to next process
+    }
+
+    CloseHandle(snapshot);
+    return exists;
+}
 
 int main(int argc, char* argv[]){
 
     //benchmark start
-    auto benchamrk_start = std::chrono::high_resolution_clock::now();
+    //auto benchamrk_start = std::chrono::high_resolution_clock::now();
 
+    std::cout << IsProcessRunning(L"harmony.exe") << std::endl;
+
+    /*
     try{
         InputStructure i = InputStructure(argc, argv);
         auto benchmark_end = std::chrono::high_resolution_clock::now();
@@ -22,6 +52,7 @@ int main(int argc, char* argv[]){
     } catch(const std::runtime_error& e){
         std::cout << Color::RED << "ERROR: " << e.what() << Color::RESET << std::endl;
     }
+    */
     
     //std::unordered_multimap<enum argument_type, std::string> argument_map;
 
--- main_daemon.cpp
+++ main_daemon.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Hello World!" << std::endl;
+    std::cout << "[Press Q to quit]" << std::endl;
+    char tst = getchar();
+    while(tst != 'q'){
+        tst = getchar();
+    }
+    std::cout << "Quiting..." << std::endl;
     return 0;
 }
