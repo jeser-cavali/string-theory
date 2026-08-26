d8f0058608f069bee972b88fb5e74090b3d60dc0
xXQuasarXx <stelarfield@protonmail.com>
2026-08-26 19:01:46 -0300

    0.2.3b - Fix CmakeLists.txt

--- lib/color.hpp
+++ lib/color.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string_view>
+#include <iostream>
 
 namespace Color {
     constexpr std::string_view RESET = "\033[0m";
@@ -8,20 +9,20 @@ namespace Color {
     constexpr std::string_view GREEN = "\033[32m";
     constexpr std::string_view YELLOW = "\033[33m";
     constexpr std::string_view BLUE = "\033[34m";
-}
+};
 
 inline void printError(std::string_view string){
     std::cout << Color::RED << string << Color::RESET << std::endl;
-}
+};
 
 inline void printAlert(std::string_view string){
     std::cout << Color::YELLOW << string << Color::RESET << std::endl;
-}
+};
 
 inline void printSuccess(std::string_view string){
     std::cout << Color::GREEN << string << Color::RESET << std::endl;
-}
+};
 
 inline void printTooltip(std::string_view string){
     std::cout << Color::BLUE << string << Color::RESET << std::endl;
-}
+};
--- main_cli.cpp
+++ main_cli.cpp
@@ -1,9 +1,9 @@
-#include "./build/_deps/harmony-src/harmony.hpp"
+//#include "./build/_deps/harmony-src/harmony.hpp"
 #include "color.hpp"
 
 #include <iostream>
 
 int main(int argc, char* argv[]){
-    std::cout << Color::RED << "teste" << Color::RESET << std::endl;
+    
     return 0;
 }
