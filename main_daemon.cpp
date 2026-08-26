d33bd71f7674bb3921f7ef4923b2100093e6b1b6
xXQuasarXx <stelarfield@protonmail.com>
2026-08-26 18:15:32 -0300

    0.2.2 - Src code cleaning

--- main_daemon.cpp
+++ main_daemon.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
 
 int main(){
-    std::cout << "[Press Q to quit]" << std::endl;
-    char tst = getchar();
-    while(tst != 'q'){
-        tst = getchar();
-    }
-    std::cout << "Quiting..." << std::endl;
+    
     return 0;
 }
