ef6e50057ff83ecd812a444ab94e06c80dd75125
xXQuasarXx <stelarfield@protonmail.com>
2026-08-26 18:44:24 -0300

    0.2.3 - New functions to color.cpp

--- lib/color.hpp
+++ lib/color.hpp
@@ -9,3 +9,19 @@ namespace Color {
     constexpr std::string_view YELLOW = "\033[33m";
     constexpr std::string_view BLUE = "\033[34m";
 }
+
+inline void printError(std::string_view string){
+    std::cout << Color::RED << string << Color::RESET << std::endl;
+}
+
+inline void printAlert(std::string_view string){
+    std::cout << Color::YELLOW << string << Color::RESET << std::endl;
+}
+
+inline void printSuccess(std::string_view string){
+    std::cout << Color::GREEN << string << Color::RESET << std::endl;
+}
+
+inline void printTooltip(std::string_view string){
+    std::cout << Color::BLUE << string << Color::RESET << std::endl;
+}
